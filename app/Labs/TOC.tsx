5cf2d1403410c3f61db1659aef03d1b46ac18b91
songxinru378 <songflorence4@gmail.com>
2025-10-06 22:11:25 -0400

    Update TOC.tsx

--- app/Labs/TOC.tsx
+++ app/Labs/TOC.tsx
@@ -1,28 +1,4 @@
-/*import Link from "next/link";
-export default function TOC() {
- return (
-   <ul>
-     <li>
-       <Link href="/Labs" id="wd-lab1-link">
-         Home </Link>
-     </li>
-     <li>
-       <Link href="/Labs/Lab1" id="wd-lab1-link">
-         Lab 1 </Link>
-     </li>
-     <li>
-       <Link href="/Labs/Lab2" id="wd-lab2-link">
-         Lab 2 </Link>
-     </li>
-     <li>
-       <Link href="/Labs/Lab3" id="wd-lab3-link">
-         Lab 3 </Link>
-     </li>
-     <li>
-       <Link href="/" id="wd-lab3-link">
-         Kambaz </Link> </li>
-   </ul>
-);}*/
+
 
 import { Nav, NavItem, NavLink } from "react-bootstrap";
 import Link from "next/link";
@@ -45,7 +21,7 @@ export default function TOC() {
        <NavLink href="/" as={Link}>Kambaz</NavLink>
      </NavItem>
      <NavItem>
-       <NavLink href="https://github.com/jannunzi">My GitHub</NavLink>
+       <NavLink href="https://github.com/songxinru378/kanbas-next-js.git">My GitHub</NavLink>
      </NavItem>
    </Nav>
 );}
