84758b75fc2d74e55f6ab960d530c7990719c87a
songxinru378 <songflorence4@gmail.com>
2025-10-06 22:02:12 -0400

    Update page.tsx

--- app/(Kambaz)/Account/Signup/page.tsx
+++ app/(Kambaz)/Account/Signup/page.tsx
@@ -3,13 +3,7 @@ import { FormControl } from "react-bootstrap";
 export default function Signup() {
   return (
     <div id="wd-signup-screen">
-      {/*<h3>Sign up</h3>
-      <input placeholder="username" className="wd-username" defaultValue={"Sirius Black"}/><br/>
-      <input placeholder="password" type="password" className="wd-password" defaultValue={"123456"}/><br/>
-      <input placeholder="verify password"
-             type="password" className="wd-password-verify" defaultValue={"123456"}/><br/>
-      <Link  href="Profile" > Sign up </Link><br />
-      <Link  href="Signin" > Sign in </Link>*/}
+      
 
       <h1>Sign Up</h1>
       <FormControl id="wd-username"
