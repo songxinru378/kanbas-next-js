f123496c0bf6041465e79368bcff0f1a530dd5bc
songxinru378 <songflorence4@gmail.com>
2025-11-08 16:30:32 -0500

    Update TodoList.tsx

--- app/Labs/Lab4/ReduxExamples/todos/TodoList.tsx
+++ app/Labs/Lab4/ReduxExamples/todos/TodoList.tsx
@@ -15,29 +15,5 @@ export default function TodoList() {
         {todos.map((todo: any) => (
           <TodoItem todo={todo} />
         ))}
-
-
-        {/*<ListGroupItem className="d-flex align-items-center">
-          <FormControl className="me-2" value={todo.title}
-            onChange={(e) => setTodo({ ...todo, title: e.target.value })}/>
-        <div className="ms-auto flex-nowrap d-flex">
-          <Button className="me-2" variant="warning" onClick={() => updateTodo(todo)}
-                  id="wd-update-todo-click"> Update </Button>
-          <Button className="me-2" variant="success" onClick={() => addTodo(todo)}
-                  id="wd-add-todo-click"> Add </Button>
-          </div>
-        </ListGroupItem>
-        {todos.map((todo) => (
-          <ListGroupItem key={todo.id} className="d-flex align-items-center">
-            {todo.title}
-            <div className="ms-auto">
-                <Button className="me-2" variant="primary" onClick={() => setTodo(todo)}
-                    id="wd-set-todo-click"> Edit </Button>
-                <Button className="me-2" variant="danger" onClick={() => deleteTodo(todo.id)}
-                    id="wd-delete-todo-click"> Delete </Button>
-            
-            </div>
-          </ListGroupItem>
-        ))}*/}
       </ListGroup><hr/>
 </div>);}
