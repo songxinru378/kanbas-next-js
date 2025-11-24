31e87425ecbd596e026d640979bdd2ad180a11fa
songxinru378 <songflorence4@gmail.com>
2025-11-24 17:53:37 -0500

    Update page.tsx

--- app/(Kambaz)/Dashboard/page.tsx
+++ app/(Kambaz)/Dashboard/page.tsx
@@ -109,6 +109,7 @@ export default function Dashboard() {
                     </button>
                         </>
                     )}
+                <br/>
 
                     {currentUser && (
                         enrolled ? (
